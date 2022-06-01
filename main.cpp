b39b55b802a263961e8841a43f1ee6f989bcf82f
PJH594 <hochbaumjacob707@gmail.com>
2022-06-02 00:15:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//g++ -o main.exe main.cpp -IC:\xerces-c\include -LC:\xerces-c\lib -lxerces-c.dll -std=c++11
 #include <iostream>
 #include <vector>
 
@@ -83,6 +84,9 @@ struct GetOptSetup {
 DOMDocument* GetDomDocument(std::string xmlName);
 GetOptSetup ParseDOMToDataStrct(DOMDocument* doc);
 Author GetAuthor(DOMDocument* doc);
+OverAllDescription GetOverAllDescription(DOMDocument* doc);
+SampleUsage GetSampleUsage(DOMDocument* doc);
+Options GetOptions(DOMDocument* doc);
 
 int main() {
 
@@ -108,11 +112,137 @@ GetOptSetup ParseDOMToDataStrct(DOMDocument* doc){
     data.sourceFileName.content = toCh(doc->getElementsByTagName(toXMLCh("SourceFileName"))->item(0)->getTextContent());
     data.nameSpace.content = toCh(doc->getElementsByTagName(toXMLCh("NameSpace"))->item(0)->getTextContent());
     data.className.content = toCh(doc->getElementsByTagName(toXMLCh("ClassName"))->item(0)->getTextContent());
-    //OverAllDescription fehlt
-    //SampleUsage fehlt
-    //Options fehlt
+    data.overAllDescription = GetOverAllDescription(doc);
+    data.sampleUsage = GetSampleUsage(doc);
+    data.options = GetOptions(doc);
 
     return data;
+}
+
+/*
+ * std::string ref;
+    std::string shortOpt;
+    std::string longOpt;
+    std::string exclusion;
+    std::string connectToInternalMethod;
+    std::string description;
+    std::string interface;
+    std::string hasArguments;
+ */
+
+Options GetOptions(DOMDocument* doc)
+{
+    Options o = Options();
+
+    DOMNodeList* optionList = doc->getElementsByTagName(toXMLCh("Option")); //Hole alle Nodes die "Author" heißen
+    for ( XMLSize_t i = 0; i < optionList->getLength(); i++ ) { //gehe durch diese liste durch
+        if(optionList->item(i)->hasAttributes()){ //Schaue ob Author von (i) Attribute hat wie z.B Name, Email, Phone
+            DOMNamedNodeMap *AttrMap = optionList->item(i)->getAttributes(); // Wenn ja erstelle einen Map von diesen Attributen
+
+            Option option = Option();
+
+            for ( XMLSize_t j=0; j<AttrMap->getLength(); j++) { // gehe durch diese Map mit For loop durch
+
+
+
+                string nodeName = toCh(AttrMap->item(j)->getNodeName());// attribut von xmlchar zu String convertieren um vergleiche zu können
+
+                if(nodeName == "Ref"){ //Vergleiche ob Attribut mail heißt
+                    option.ref = toCh(AttrMap->item(j)->getTextContent()); // schreibe den content von Attribut Mail in a.mail
+                }else if(nodeName == "ShortOpt"){
+                    option.shortOpt = toCh(AttrMap->item(j)->getTextContent());
+                }else if(nodeName == "LongOpt"){
+                    option.longOpt = toCh(AttrMap->item(j)->getTextContent());
+                }else if(nodeName == "Exclusion"){
+                    option.exclusion = toCh(AttrMap->item(j)->getTextContent());
+                }else if(nodeName == "ConnectToInternalMethod"){
+                    option.connectToInternalMethod = toCh(AttrMap->item(j)->getTextContent());
+                }else if(nodeName == "Description"){
+                    option.description = toCh(AttrMap->item(j)->getTextContent());
+                }else if(nodeName == "Interface"){
+                    option.interface = toCh(AttrMap->item(j)->getTextContent());
+                }else if(nodeName == "HasArguments"){
+                    option.hasArguments = toCh(AttrMap->item(j)->getTextContent());
+                }
+            }
+            o.addOption(option);
+        }else{
+            cout << "Es wurden keine Informationen zu Optionen hinterlegt!" << endl;
+        }
+    }
+    return o;
+}
+
+SampleUsage GetSampleUsage(DOMDocument* doc)
+{
+    SampleUsage sA = SampleUsage();
+
+    DOMNodeList* sampleUsageList = doc->getElementsByTagName(toXMLCh("Sample")); //Hole alle Nodes die "Author" heißen
+
+
+    for ( XMLSize_t i=0;i<sampleUsageList->getLength(); i++)
+    {
+        //cout << "textCont: " << toCh(blockList->item(i)->getTextContent())<< endl;
+
+        Sample sample = Sample();
+
+        sample.content = toCh(sampleUsageList->item(i)->getTextContent());
+
+        sA.addSample(sample);
+
+
+    }
+
+    return sA;
+}
+
+
+
+
+OverAllDescription GetOverAllDescription(DOMDocument* doc)
+{
+    OverAllDescription oA = OverAllDescription();
+
+         //Passt nur, OverAllDescription das einzige element mit "Block"-Elementen ist, da alle BlockElemente hier geladen werden, aus der ganzen Datei
+         DOMNodeList* blockList = doc->getElementsByTagName(toXMLCh("Block")); //Alle Nodes die Block heißen
+
+         for ( XMLSize_t i=0;i<blockList->getLength(); i++)
+         {
+             //cout << "textCont: " << toCh(blockList->item(i)->getTextContent())<< endl;
+
+             Block block = Block();
+
+             block.content = toCh(blockList->item(i)->getTextContent());
+
+             oA.addBlock(block);
+
+         }
+
+
+    return oA;
+
+
+
+
+        /*for(XMLSize_t i = 0; i < blockList->getLength(); i++) //Alle Block_Elemente durchgehen
+        {
+            cout<<"2"<<endl;
+            if(blockList->item(i)->hasAttributes())
+            {
+                cout<<"3"<<endl;
+                DOMNamedNodeMap *AttrMap = blockList->item(i)->getAttributes();
+
+                for(XMLSize_t j =0; j<AttrMap->getLength(); j++)
+                {
+                    cout<<"4"<<endl;
+                    string blockText = toCh(AttrMap->item(j)->getTextContent());
+                    cout << "TEST_TEXT" << blockText << endl;
+                }
+
+            }
+
+        }*/
+
 }
 
 Author GetAuthor(DOMDocument* doc){
@@ -126,7 +256,7 @@ Author GetAuthor(DOMDocument* doc){
             for ( XMLSize_t j=0; j<AttrMap->getLength(); j++) { // gehe durch diese Map mit For loop durch
                 
                 string nodeName = toCh(AttrMap->item(j)->getNodeName());// attribut von xmlchar zu String convertieren um vergleiche zu können 
-                if(nodeName == "Mail"){ //Vergleiche ob Attibut mail heißt 
+                if(nodeName == "Mail"){ //Vergleiche ob Attibut mail heißt
                     a.mail = toCh(AttrMap->item(j)->getTextContent()); // schreibe den content von Attribut Mail in a.mail
                 }else if(nodeName == "Phone"){
                     a.phone = toCh(AttrMap->item(j)->getTextContent());
