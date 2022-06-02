fced2b478a3ba1b51a3b119de7985de0863c2caf
xKnopix <michael.hornstein9@gmail.com>
2022-06-02 17:35:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include "xml_parser.h"
+//Wichtig! Unter File Preferences Settings Extensions "Run Code configuration" "Executor Map" "Edit in settings.json" 
+//nach g++ $file weg machen und dafür main.cpp xml_parser.cpp xml_parser.h einfügen, sonst geht der code runner Button nicht mehr 
+//hat den grund dass beim Compelieren alle Dateien mit angegeben werden müssen und nicht nur die main.cpp
 
 int main() {
     xml_parser x;
