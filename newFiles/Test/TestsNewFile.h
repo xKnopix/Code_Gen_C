c3665af6bddbed2a216d39be2e66c6603ed5321a
xKnopix <58215384+xKnopix@users.noreply.github.com>
2022-07-14 15:50:42 +0200

    Add files via upload

---
+++ newFiles/Test/TestsNewFile.h
@@ -0,0 +1,52 @@
+#ifndef NEWFILE_H
+#define NEWFILE_H
+#include <string>
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+    class noClassname
+    {  
+    bool VersionBool;
+    bool outpathBool;
+    string outpathParam;
+    bool astylepathBool;
+    string astylepathParam;
+    int signperlineInt = 79;
+    bool signperlineBool;
+    string signperlineParam = "79";
+    bool onlyifnewerBool;
+    bool noformatBool;
+    bool parseonlyBool = true;
+    void myPrint(string myString, int newLineChecker);
+    protected:
+    virtual void  printHelp(int signPerLine);
+    public:
+    bool isSetVersion();
+    public:
+    bool isSetoutpath();
+    public:
+    string getValueOfoutpath();
+    public:
+    bool isSetastylepath();
+    public:
+    string getValueOfastylepath();
+    public:
+    bool isSetsignperline();
+    public:
+    int getValueOfsignperline();
+    public:
+    bool isSetonlyifnewer();
+    public:
+    bool isSetnoformat();
+    public:
+    void ParseXML(bool parseonlyBool);
+    public:
+    virtual void hallo(bool b) = 0;
+    public:
+    virtual void unknownOption(const string& unknown_option);
+    void parse(int argc, char* argv[]);
+    };
+#endif
