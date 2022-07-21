367a77c2d45a8654bb8a865c6c617a3759954a02
xKnopix <michael.hornstein9@gmail.com>
2022-07-21 20:20:13 +0200

    c

--- C_Code_Gen/mainGenerator.cpp
+++ C_Code_Gen/mainGenerator.cpp
@@ -42,6 +42,10 @@ void mainGenerator::genInheriteClass(xml_parser::Options options, xml_parser::Cl
     }
 
     if(s.size() >0){
+        
+        this->output.append("///Diese Klasse erbt von");
+        this->output.append(classN.content);
+        this->output.append(" , hier können die Externen Methoden überschrieben werden\n");
 
         this->output.append("class Klasse : public ");
         this->output.append(classN.content);
@@ -61,7 +65,9 @@ void mainGenerator::genInheriteClass(xml_parser::Options options, xml_parser::Cl
                     this->output.append("string s");
                 }
             }
-            this->output.append("){\n\tcout << \"Example implementation of external Method\" << endl;\n}\n\n");
+            this->output.append("){");
+            this->output.append("///Die Methode wird vererbt und soll überschrieben werden");
+            this->output.append("\n\tcout << \"Example implementation of external Method\" << endl;\n}\n\n");
         }
         this->output.append("};\n\n");
     }
